b309888472086fcec6a108e060df02e09a38fdf8
Ashutosh Mohanty <ashutoshmohanty1799@gmail.com>
2022-10-05 22:57:34 +0530

    test files added

--- app.py
+++ app.py
@@ -36,30 +36,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-
-    if request.method == "POST":
-        try:
-            if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
-                return render_template("index.html", response=response)
-            elif request.json:
-                response = api_response(request.json)
-                return jsonify(response)
-
-        except Exception as e:
-            print(e)
-            error = {"error": "Something went wrong!! Try again later!"}
-            error = {"error": e}
-
-            return render_template("404.html", error=error)
-    else:
-        return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
